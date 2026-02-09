0c109c8af9678191be79b9535c164b16e72251fc
Hippophile <paschalakisfilip@gmail.com>
2026-02-09 12:38:46 +0200

    Add high score tracking to scoreboard functionality

--- Day-020/scoreboard.py
+++ Day-020/scoreboard.py
@@ -2,8 +2,8 @@ from turtle import Turtle
 
 COLOR = "gold"
 
+
 class Scoreboard(Turtle):
-    
 
     def __init__(self):
         super().__init__()
@@ -26,6 +26,20 @@ class Scoreboard(Turtle):
         self.color("gold")
         text = f"Game Over\n   Score: {self.score}"
         self.write(text, align="center", font=('Arial', 28, 'normal'))
+
+        file = open("high_score.txt", "r")
+        HIGH_SCORE = file.read()
+        file.close()  
+        if self.score > int(HIGH_SCORE):
+            self.goto(0, 150)
+            text_hs = f"Wow New High Score: {self.score}"
+            self.write(text_hs, align="center", font=('Arial', 28, 'normal'))
+            HIGH_SCORE = self.score
+            file = open("high_score.txt", "w")
+            file.write(str(HIGH_SCORE))
+            file.close()
+
+
 
 
     
